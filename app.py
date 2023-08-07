1c688ea107e750faebe11d6be72b121c43ef710a
IndigoWizard <microclubist@gmail.com>
2023-08-07 01:47:54 +0100

    GEE config
    - Setting up GEE lib
    - Configuring GEE drawing method
    - Hooking GEE drawing method to Folium
    - adding map layer control

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
+import ee
+from ee import image
 import folium
 from folium import WmsTileLayer
 import webbrowser
+
+# command line > activate environement > earthengine authenticate
+# Or: Uncomment then execute only once to authenticate (put back as comment)
+#ee.Authenticate()
+
+# initializing the earth engine library
+ee.Initialize()
+
+# ##### earth-engine drawing method setup
+def add_ee_layer(self, ee_image_object, vis_params, name):
+  map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
+  layer = folium.raster_layers.TileLayer(
+      tiles = map_id_dict['tile_fetcher'].url_format,
+      attr = 'Map Data &copy; <a href="https://earthengine.google.com/">Google Earth Engine</a>',
+      name = name,
+      overlay = True,
+      control = True
+  )
+  layer.add_to(self)
+  return layer
+
+# configuring earth engine display rendering method in folium
+folium.Map.add_ee_layer = add_ee_layer
 
 # setting up main map
 m = folium.Map(location = [36.8, 3.0], tiles='Open Street Map', zoom_start = 5, control_scale = True)
@@ -17,6 +42,10 @@ b2 = WmsTileLayer(
   attr='Topography Map',
   show=False
 ).add_to(m)
+
+
+# Folium Map Layer Control: we can see and interact with map layers
+folium.LayerControl(collapsed=False).add_to(m)
 
 # saving output file as html
 m.save('index.html')
